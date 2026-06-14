a2960dadf7c59c178e2bd628a5120cd7f9feafe1
Auto Commit <auto@local>
2026-06-14 22:23:11 +0530

    Auto: package.json updated @ 2026-06-14T16:53:11.195Z

--- scripts/build-github-pages.js
+++
@@ -1,6 +0,0 @@
-import { execSync } from "node:child_process";
-
-process.env.GITHUB_PAGES = "true";
-process.env.GITHUB_REPOSITORY ??= "TSaiPradyumna/SSSIHL_COE_DFNS_NEW";
-
-execSync("vite build", { stdio: "inherit" });
